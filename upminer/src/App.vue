c5ff11e299017c51ef6be8deb504d44fc7b2019e
Luis <luisfelipegoulart94@gmail.com>
2021-08-15 19:12:04 -0300

    Resetar do navegador estilos globalmente

--- upminer/src/App.vue
+++ upminer/src/App.vue
@@ -7,3 +7,14 @@
 <script>
 export default {}
 </script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  font-weight: 400;
+  font-family: "Roboto", sans-serif;
+}
+</style>
