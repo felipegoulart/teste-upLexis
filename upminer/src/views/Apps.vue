716e8f6911642dbd0fef810553e66155da15e54a
Luis <luisfelipegoulart94@gmail.com>
2021-08-15 15:26:38 -0300

    Adicionados os componentes navbar e order Adequação das classes ao padrão BEM

--- upminer/src/views/Apps.vue
+++ upminer/src/views/Apps.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="home">
-    <main class="app-list">
-      <card class="app-card" v-for="app in apps" :key="app.id" :card="app"/>
-    </main>
+    <header>
+      <categories-navbar />
+      <order class="order"/>
+    </header>
+    <section class="app-list">
+      <card class="app-list__card" v-for="app in appList" :key="app.id" :card="app" />
+    </section>
   </div>
 </template>
 
 <script>
 import categories from '@/storage/categories'
 import apps from '@/storage/appList'
-import Card from '@/components/card/Card'
+
+import CategoriesNavbar from "@/components/categories/CategoriesNavbar";
+import Order from "@/components/order/Order";
+import Card from "@/components/card/Card";
 
 export default {
   name: 'Home',
@@ -32,20 +39,29 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  
+
   font-size: $text-size-1;
   font-weight: 500;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
+}
+
+.order {
+  margin-top: 36px;
+  margin-bottom: 9px;
 }
 
 .app-list {
   padding: 0 16px;
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-evenly;
-  
-  .app-card {
+
+  @media screen and (max-width: 480px) {
+    justify-content: space-evenly;
+  }
+
+  .app-list__card {
     margin: 27px 12px;
   }
-}
+  }
+  
 </style>
