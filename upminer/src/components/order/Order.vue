eadf23124e5d85bcdd242b005b532d0052791e0b
Luis <luisfelipegoulart94@gmail.com>
2021-08-15 15:22:21 -0300

    Criado o componente de ordenação

---
+++ upminer/src/components/order/Order.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <label class="order__label" for="order">Ordenar</label>
+    <select class="order__selector" name="order" id="order" v-model="order">
+      <option value="Lançamento">Lançamento</option>
+      <option value="Preço">Preço</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'order',
+
+  data() {
+    return {
+      order: 'Lançamento'
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.order__label {
+  font-weight: bold;
+  text-transform: uppercase;
+  margin: 20px;
+}
+
+.order__selector {
+  width: 220px;
+  height: 34px;
+
+  border: 2px solid $color-gray-2;
+  border-radius: 5px
+}
+
+</style>
