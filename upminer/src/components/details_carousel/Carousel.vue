eaba49cc8453f132d39cdbf84013364261f5b551
Luis <luisfelipegoulart94@gmail.com>
2021-08-15 19:11:40 -0300

    Criado carrossel de imagens

---
+++ upminer/src/components/details_carousel/Carousel.vue
@@ -0,0 +1,66 @@
+<template>
+  <div
+    class="carousel"
+  >
+    <i class="fas fa-arrow-left carousel__arrow carousel__arrow--left"></i>
+    <carousel-item
+      v-for="(item, i) in items"
+      :key="i"
+      :carousel="item"
+    />
+    <i class="fas fa-arrow-right carousel__arrow carousel__arrow--right"></i>
+  </div>
+</template>
+
+<script>
+import CarouselItem from "./CarouselItem";
+
+export default {
+  name: "carousel",
+
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+
+  components: {
+    CarouselItem,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.carousel {
+  display: flex;
+  width: 90%;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+
+  &__arrow {
+    color: $color-gray-3;
+    font-size: $text-size-4;
+    
+    position: absolute;
+    top: 50%;
+
+    &:hover {
+      color: $color-black;
+      cursor: pointer;
+    }
+
+    &--left {
+      left: -30px;
+      transform: translate(-50%, -50%);
+    }
+
+    &--right {
+      right: -30px;
+      transform: translate(50%, -50%);
+    }
+  }
+}
+
+</style>
---
+++ upminer/src/components/details_carousel/CarouselItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="carousel__item">
+    <img class="carousel__image" :src="image" alt="Imagem do aplicativo">
+    <p class="carousel__description">{{carousel.description}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "carousel-item",
+
+  props: {
+    carousel: {
+      type: Object,
+      default: () => ({}),
+      required: true
+    },
+  },
+
+  computed: {
+    image() {
+      return this.carousel.img
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.carousel {
+  &__item {
+    background-color: $color-white;
+    width: 488px;
+    max-width: 488px;
+    height: 320px; 
+    max-height: 320px; 
+    padding: 10px;
+    border-radius: 5px;
+    box-shadow: 0 0 18px rgba($color-black, 0.1);
+  }
+
+  &__description{
+    color: $color-orange-1;
+    font-size: $text-size-2;
+    font-weight: 400;
+    margin-top: 20px;
+  }
+}
+</style>
