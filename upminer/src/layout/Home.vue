8b64b303c006fc3697b33b7f8e4e8002b3d58b90
Luis <luisfelipegoulart94@gmail.com>
2021-08-15 15:18:40 -0300

    Lib Vue-Agile adicionada ao projeto

--- upminer/src/layout/Home.vue
+++ upminer/src/layout/Home.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <agile
+      class="carousel"
+      :dots="false"
+      :navButtons="false"
+      :infinite="true"
+      :autoplay="true"
+      :autoplaySpeed="6000"
+      :speed="1000"
+    >
+      <img
+        class="slide"
+        src="../../public/banner/header__banner-1.png"
+        alt=""
+      />
+      <img
+        class="slide"
+        src="../../public/banner/header__banner-2.png"
+        alt=""
+      />
+    </agile>
     <router-view />
   </div>
 </template>
