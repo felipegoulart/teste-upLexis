e78cf6143babd1261cf96a0e6c6ca47e823b057a
Luis <luisfelipegoulart94@gmail.com>
2021-08-15 19:12:39 -0300

    Criado componente responsável por voltar a página

---
+++ upminer/src/components/backTo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="back" @click="back">
+    <i class="fas fa-chevron-left back__arrow"></i><span>{{myApp.title || 'Voltar'}}</span>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'back-to',
+
+  computed: {
+    ...mapGetters(['myApp'])
+  },
+
+  methods: {
+    back() {
+      this.$router.back(1)
+    }
+  },
+
+}
+</script>
+
+<style lang="scss">
+.back {
+  color: $color-orange-1;
+  font-size: $text-size-6;
+  cursor: pointer;
+
+  &__arrow {
+    margin-right: 18px;
+  }
+
+}
+</style>
